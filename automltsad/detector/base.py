c06bf2847c0fc245729b1453ae0ec053b5abb4e3
NevoleMarek <nevole.marek@gmail.com>
2022-12-17 20:03:13 +0100

    Add docstrings and abstract predict method

--- automltsad/detector/base.py
+++ automltsad/detector/base.py
@@ -2,10 +2,31 @@ from abc import ABC, abstractmethod
 
 
 class BaseDetector(ABC):
+    '''
+    Base abstract class for detectors.
+    '''
+
     @abstractmethod
     def fit(self):
+        '''
+        Abstract method for fitting of a detector.
+        '''
+        pass
+
+    @abstractmethod
+    def predict(self):
+        '''
+        Abstract method for predictions of a detector.
+
+        Should return labels.
+        '''
         pass
 
     @abstractmethod
     def predict_anomaly_scores(self):
+        '''
+        Abstract method for predictions of anomaly scores of a detector.
+
+        Should return anomaly scores.
+        '''
         pass
