dfe39e91a2cfbee75206af334841093860b35488
NevoleMarek <nevole.marek@gmail.com>
2022-12-19 13:04:43 +0100

    Add check_one_sample validation

--- automltsad/validation/__init__.py
+++ automltsad/validation/__init__.py
@@ -1,5 +1,6 @@
 from automltsad.validation.validator import (
     check_if,
     check_if_fitted,
+    check_one_sample,
     validate_data_format,
 )
--- automltsad/validation/validator.py
+++ automltsad/validation/validator.py
@@ -6,7 +6,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def validate_data_format(X: np.ndarray):
-    _LOGGER.info('Validating data')
+    _LOGGER.info('Validating_data_format')
     n_dims = len(X.shape)
     if n_dims != 3:
         raise Exception(
@@ -26,3 +26,10 @@ def check_if(obj, attr):
 
 def check_if_fitted(obj):
     return check_if(obj, '_fitted')
+
+
+def check_one_sample(X: np.ndarray):
+    _LOGGER.info('check_one_sample')
+    n_samples = X.shape[0]
+    if n_samples != 1:
+        raise Exception('More than 1 sample in data')
