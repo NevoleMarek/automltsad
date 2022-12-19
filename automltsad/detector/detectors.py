9cb89ce25d54247ba6c0b42cc717929f735f2e83
NevoleMarek <nevole.marek@gmail.com>
2022-12-19 13:25:24 +0100

    Final TrivialDetector implementation

--- automltsad/detector/detectors.py
+++ automltsad/detector/detectors.py
@@ -3,32 +3,100 @@ import logging
 import numpy as np
 
 from automltsad.detector import BaseDetector
+from automltsad.validation import (
+    check_if_fitted,
+    check_one_sample,
+    validate_data_format,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def check_if_fitted(model):
-    if not model._fitted:
-        raise Exception(f'{model.__class__.__name__} is not fitted.')
-
-
 class TrivialDetector(BaseDetector):
-    def __init__(self) -> None:
+    '''
+    TrivialDetector is a simple detector implementation.
+
+    TrivialDetector standardizes data based on mean and std of training data.
+    Anomaly score is the number of stds from mean value of training data.
+    Threshold is determined as 1-contamination-th quantile of standardized
+    data.
+
+
+    Parameters
+    ----------
+    contamination : float
+        Contamination parameter is used to select threshold.
+    '''
+
+    def __init__(self, contamination: float) -> None:
         self._fitted = False
+        self.contamination = contamination
 
     def fit(self, X: np.ndarray):
+        '''
+        Fit the trivial detector on training dataset.
+
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_timepoints, n_features)
+            Training data.
+
+        Returns
+        -------
+        self: TrivialDetector
+            The fitted trivial detector.
+        '''
         _LOGGER.info(f'Fitting {self.__class__.__name__}')
+        validate_data_format(X)
+        check_one_sample(X)
         self._fitted = True
-        self._mean = np.mean(X)
-        self._std = np.std(X)
-        self._threshold = np.quantile((X - self._mean) / self._std, 0.99)
+        self._mean = np.mean(X, axis=1)
+        self._std = np.std(X, axis=1)
+        self._threshold = np.quantile(
+            (X - self._mean) / self._std, 1 - self.contamination
+        )
+        return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        _LOGGER.info(f'Predicting {self.__class__.__name__}')
-        scores = self.predict_anomaly_scores(X)
-        return np.abs(scores) > self._threshold
+        '''
+        Predict anomaly labels.
 
-    def predict_anomaly_scores(self, X: np.ndarray) -> np.ndarray:
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_timepoints, n_features)
+            Data
+
+        Returns
+        -------
+        np.ndarray
+            np.ndarray of shape (n_samples, n_timepoints, n_features)
+            Labels of data points
+        '''
+        _LOGGER.info(f'Predicting {self.__class__.__name__}')
+        validate_data_format(X)
+        check_one_sample(X)
         check_if_fitted(self)
 
-        return (X - self._mean) / self._std
+        scores = self.predict_anomaly_scores(X)
+        return scores > self._threshold
+
+    def predict_anomaly_scores(self, X: np.ndarray) -> np.ndarray:
+        '''
+        Predict anomaly scores.
+
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_timepoints, n_features)
+            Data
+
+        Returns
+        -------
+        np.ndarray
+            Array of shape (n_samples, n_timepoints, n_features)
+            Anomaly scores.
+        '''
+        validate_data_format(X)
+        check_one_sample(X)
+        check_if_fitted(self)
+
+        return np.abs((X - self._mean) / self._std)
