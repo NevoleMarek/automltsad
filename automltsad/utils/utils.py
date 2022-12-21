7d5d301fadf30d431d589e7dd59ed5343edfd746
NevoleMarek <nevole.marek@gmail.com>
2022-12-21 14:40:06 +0100

    Use of sliding_window_view in sliding_window_sequences and reshape to comply to data format

--- automltsad/utils/utils.py
+++ automltsad/utils/utils.py
@@ -1,6 +1,7 @@
 import logging
 
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -9,10 +10,8 @@ def sliding_window_sequences(
     data: np.ndarray,
     window_size: int,
 ):
-    output = []
-    for i in range(data.shape[0] - window_size + 1):
-        output.append(data[i : (i + window_size)])
-
-    output = np.stack(output)
+    n_samples, n_timepoints, n_features = data.shape
+    output = sliding_window_view(data, window_shape=window_size, axis=1)
+    output = output.reshape(-1, window_size * n_features)
     _LOGGER.info(f'Rolling window op: Shape of output {output.shape}')
     return output
