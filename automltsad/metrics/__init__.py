79caf3bb7cc790d037b0672dd200e06951b6ac5f
Marek Nevole <marek.nevole@datamole.cz>
2023-01-17 15:06:59 +0100

    Add MassVolume curve, auc, MV feature subsampling auc

--- automltsad/metrics/__init__.py
+++ automltsad/metrics/__init__.py
@@ -11,5 +11,9 @@ from automltsad.metrics.metrics import (
     f1_pa,
     f1_pa_auc_score,
     f1_pa_curve,
+    feature_subsampling_auc,
+    mass_volume_auc_score,
+    mass_volume_curve,
+    mv_feature_subsampling_auc_score,
     point_adjust,
 )
--- automltsad/metrics/metrics.py
+++ automltsad/metrics/metrics.py
@@ -1,3 +1,6 @@
+from functools import partial
+from typing import Tuple
+
 import numpy as np
 from sklearn.metrics import auc, f1_score, precision_recall_curve, roc_curve
 
@@ -126,3 +129,200 @@ def f1_pa_auc_score(labels: np.ndarray, scores: np.ndarray, threshold: float):
     '''
     k, f1 = f1_pa_curve(labels, scores, threshold)
     return auc(k, f1)
+
+
+def mass_volume_curve(
+    detector,
+    X: np.ndarray,
+    scores: np.ndarray = None,
+    alpha_min: float = 0.9,
+    alpha_max: float = 0.999,
+    alphas_count: int = 100,
+    mc_samples_count: int = 32768,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Compute mass-volume curve
+
+    Parameters
+    ----------
+    detector : object
+        detector object
+    X : np.ndarray
+        input feature matrix, usually test data
+    scores : np.ndarray, optional
+        scores for the input feature matrix, by default None
+    alpha_min : float, optional
+        minimum threshold, by default 0.9
+    alpha_max : float, optional
+        maximum threshold, by default 0.999
+    alphas_count : int, optional
+        number of thresholds, by default 128
+    mc_samples_count : int, optional
+        number of monte carlo samples, by default 131072
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        A tuple of two arrays, first contains the threshold (mass) values and second contains the volume values
+    """
+    if not scores:
+        scores = detector.predict_anomaly_scores(X)
+
+    # MV paper assumes that lower score corresponds to higher abnormality
+    scores_vec = -scores
+    alphas_vec = np.linspace(alpha_min, alpha_max, alphas_count)
+    ro_vec = np.quantile(scores_vec, 1 - alphas_vec)
+
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+
+    # volume of hypercube enclosing the data
+    volume = np.prod(X_max - X_min)
+
+    n_samples, n_features = X.shape
+    mc_samples = np.random.uniform(
+        X_min, X_max, (mc_samples_count, n_features)
+    )
+    scores_samples = -detector.predict_anomaly_scores(mc_samples)
+
+    mv_alpha = np.zeros_like(alphas_vec)
+    for i, ro in enumerate(ro_vec):
+        mv_alpha[i] = (scores_samples >= ro).sum() / mc_samples_count * volume
+    return alphas_vec, mv_alpha
+
+
+def mass_volume_auc_score(
+    detector,
+    X: np.ndarray,
+    scores: np.ndarray = None,
+    alpha_min: float = 0.9,
+    alpha_max: float = 0.999,
+    alphas_count: int = 100,
+    mc_samples_count: int = 32768,
+) -> float:
+    """
+    Compute the area under the mass-volume curve (AUC)
+
+    Parameters
+    ----------
+    detector : object
+        detector object
+    X : np.ndarray
+        input feature matrix. Usually test data.
+    scores : np.ndarray
+        scores for the input feature matrix, by default None
+    alpha_min : float, optional
+        minimum threshold, by default 0.9
+    alpha_max : float, optional
+        maximum threshold, by default 0.999
+    alphas_count : int, optional
+        number of thresholds, by default 128
+    mc_samples_count : int, optional
+        number of monte carlo samples, by default 131072
+
+    Returns
+    -------
+    float
+        AUC score
+    """
+    alphas, mvs = mass_volume_curve(
+        detector,
+        X,
+        scores,
+        alpha_min,
+        alpha_max,
+        alphas_count,
+        mc_samples_count,
+    )
+    return auc(alphas, mvs)
+
+
+def feature_subsampling_auc(
+    detector,
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    auc_func,
+    n_subfeatures: int = 5,
+    n_tries: int = 50,
+) -> float:
+    """
+    Compute AUC for given metric on feature subsample trained detector
+
+    Parameters
+    ----------
+    detector : object
+        detector object
+    X_train : np.ndarray
+        Training feature matrix
+    X_test : np.ndarray
+        Testing feature matrix
+    auc_func : function
+        function to compute AUC
+    n_subfeatures : int, optional
+        number of subsampled features, by default 5
+    n_tries : int, optional
+        number of subsampling tries, by default 50
+
+    Returns
+    -------
+    float
+        mean AUC across subsampling tries
+    """
+    _, n_features = X_train.shape
+    aucs = np.zeros((n_tries,))
+    for i in range(n_tries):
+        features = np.random.choice(
+            a=n_features, size=n_subfeatures, replace=False
+        )
+        X_train_ = X_train[:, features]
+        X_test_ = X_test[:, features]
+        detector.fit(X_train_)
+        aucs[i] = auc_func(detector, X_test_)
+    return np.mean(aucs)
+
+
+def mv_feature_subsampling_auc_score(
+    detector,
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    alpha_min: float = 0.9,
+    alpha_max: float = 0.999,
+    alphas_count: int = 100,
+    mc_samples_count: int = 32768,
+    n_subfeatures: int = 5,
+    n_tries: int = 50,
+) -> float:
+    """
+    Compute mean AUC of mass volume metric on feature subsample trained
+    detector
+
+    Parameters
+    ----------
+    detector : object
+        detector object
+    X_train : np.ndarray
+        Training feature matrix
+    X_test : np.ndarray
+        Testing feature matrix
+    auc_func : function
+        function to compute AUC
+    n_subfeatures : int, optional
+        number of subsampled features, by default 5
+    n_tries : int, optional
+        number of subsampling tries, by default 50
+
+    Returns
+    -------
+    float
+        mean AUC across subsampling tries
+    """
+    mvfunc = partial(
+        mass_volume_auc_score,
+        alpha_min=alpha_min,
+        alpha_max=alpha_max,
+        alphas_count=alphas_count,
+        mc_samples_count=mc_samples_count,
+    )
+    return feature_subsampling_auc(
+        detector, X_train, X_test, mvfunc, n_subfeatures, n_tries
+    )
