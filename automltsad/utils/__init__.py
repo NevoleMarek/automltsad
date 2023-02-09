1661f84a7b9949d6ef0c8abab1a30b99f4421245
NevoleMarek <marek.nevole@datamole.ai>
2023-02-09 13:48:43 +0100

    Add Autoperiod

--- automltsad/utils/__init__.py
+++ automltsad/utils/__init__.py
@@ -1,5 +1,6 @@
 from tslearn.utils import to_time_series, to_time_series_dataset
 
+from automltsad.utils.autoperiod import Autoperiod
 from automltsad.utils.utils import (
     reduce_window_scores,
     sliding_target_window_sequences,
