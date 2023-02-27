94f890471255b78c372978baa7916c841e1f24db
NevoleMarek <nevole.marek@gmail.com>
2023-02-27 11:42:30 +0100

    Add torch positional encoding

--- automltsad/detectors/utils.py
+++ automltsad/detectors/utils.py
@@ -16,20 +16,21 @@ def print_progress(ep, n_ep, t_l, v_l=None):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
+    def __init__(
+        self, d_model: int, dropout: float = 0.1, max_len: int = 5000
+    ):
+        super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(
-            torch.arange(0, d_model).float() * (-np.log(10000.0) / d_model)
+            torch.arange(0, d_model, 2) * (-np.log(10000.0) / d_model)
         )
-        pe += torch.sin(position * div_term)
-        pe += torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
-    def forward(self, x, pos=0):
-        x = x + self.pe[pos : pos + x.size(0), :]
+    def forward(self, x):
+        x = x + self.pe[: x.size(0)]
         return self.dropout(x)
