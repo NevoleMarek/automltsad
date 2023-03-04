62bd23903c7e5ec71bca940a15f1112a0341f914
NevoleMarek <nevole.marek@gmail.com>
2023-03-04 19:29:09 +0100

    Replace utils funcs and Add evaluation

--- experiments/main_unsupervised.py
+++ experiments/main_unsupervised.py
@@ -1,7 +1,6 @@
-import csv
-import datetime
 import multiprocessing
 import os
+import warnings
 from time import perf_counter
 
 import numpy as np
@@ -10,75 +9,34 @@ import tqdm
 import yaml
 from config import (
     CONFIG_DIR,
-    DATA_DIR,
-    DATASET_DIR,
     MODEL_DIR,
     NAME_TO_MODEL,
     TEST_DATASETS,
     get_hparams,
 )
-from optuna.pruners import HyperbandPruner
 from optuna.samplers import TPESampler
+from utils import (
+    get_dataset_seasonality,
+    get_yaml_config,
+    prepare_data,
+    read_file,
+    save_result,
+)
 
 from automltsad.metrics import (
+    auc,
     em_feature_subsampling_auc_score,
-    excess_mass_auc_score,
-    mass_volume_auc_score,
+    f1_pa,
+    f1_pa_auc_score,
     mv_feature_subsampling_auc_score,
+    precision_recall_curve,
+    roc_auc_score,
 )
-from automltsad.transform import MinMaxScaler
-from automltsad.utils import sliding_window_sequences, to_time_series_dataset
+from automltsad.utils import reduce_window_scores
+
+warnings.filterwarnings('ignore', 'Solver terminated early.*')
 
 EXPERIMENT = 'unsupervised'
-
-
-def get_yaml_config(path):
-    with open(path + '.yaml') as f:
-        return yaml.full_load(f)
-
-
-def read_file(fn):
-    with open(fn) as f:
-        return f.readlines()
-
-
-def get_dataset_seasonality():
-    lines = read_file(DATA_DIR + 'period.txt')
-    dct = {
-        t[0]: (t[1], t[2]) for t in [l.strip('\n').split(', ') for l in lines]
-    }
-    for k, v in dct.items():
-        if v[0] != 'None' and v[1] != 'None':
-            dct[k] = (int(v[0]), int(v[1]))
-        else:
-            dct[k] = (0, 0)
-    return dct
-
-
-def prepare_data(filename, scale, window, size):
-    test_start, anomaly_start, anomaly_end = [
-        int(i) for i in filename.split('.')[0].split('_')[-3:]
-    ]
-    filename = filename.strip('\n')
-    ts = np.loadtxt(f'{DATASET_DIR}/{filename}')
-    ts = to_time_series_dataset(ts)
-    if scale:
-        slr = MinMaxScaler()
-        ts = slr.fit_transform(ts)
-    train = ts[:, :test_start]
-    test = ts[:, test_start:]
-    label = np.zeros_like(test)
-    label[:, anomaly_start - test_start : anomaly_end - test_start] = 1
-    label = np.squeeze(label)
-    if window:
-        train, test = window_data(train, test, size)
-    return train, test, label
-
-
-def window_data(train, test, size):
-    train_w = sliding_window_sequences(train, size)
-    test_w = sliding_window_sequences(test, size)
-    return train_w, test_w
 
 
 def objective(trial, detector, dataset, det_cfg, window_sz, metric):
@@ -145,15 +103,69 @@ def process_task(task):
     return detector, dataset, metrics
 
 
-def save_result(result):
-    detector, dataset, metrics = result
-    dir_path = MODEL_DIR + f'{detector}/unsupervised/{dataset}/'
-    os.makedirs(dir_path, mode=0o777, exist_ok=True)
-    with open(
-        dir_path + 'result.yaml',
-        'w',
-    ) as file:
-        yaml.dump(metrics, file)
+def evaluate_model(scores, labels):
+    p, r, t = precision_recall_curve(labels, scores)
+    f1 = 2 * p * r / (p + r + 1e-12)
+    idx = np.nanargmax(f1)
+    step = max(len(t) // 100, 1)
+    ts = t[::step]
+    f1_pas = [f1_pa(labels, scores, th) for th in ts]
+    f1pat = f1_pa(labels, scores, t[idx])
+    idx_pa = np.nanargmax(f1_pas)
+    f1pa = f1_pas[idx_pa]
+    aucpr = auc(r, p)
+    f1_pa_t_auc = f1_pa_auc_score(labels, scores, t[idx])
+    f1_pa_ts_auc = f1_pa_auc_score(labels, scores, ts[idx_pa])
+    roc_auc = roc_auc_score(labels, scores)
+    return dict(
+        f1=f1[idx],
+        f1_pa_auc=f1_pa_t_auc,
+        f1_pa=f1pa,
+        f1_pa_ts_auc=f1_pa_ts_auc,
+        aucpr=aucpr,
+        aucroc=roc_auc,
+    )
+
+
+def process_evaluation(task):
+    detector, dataset, window_sz = task
+    # Get configs
+    hps = get_yaml_config(
+        MODEL_DIR + f'{detector}/{EXPERIMENT}/{dataset}/result'
+    )
+    det_cfg = get_yaml_config(os.path.join(MODEL_DIR, detector, 'config'))
+
+    if not window_sz:
+        window_sz = 16
+
+    # Get model
+    model = NAME_TO_MODEL[detector]
+
+    # Prepare data
+    train, test, labels = prepare_data(
+        dataset, scale=True, window=det_cfg['window'], size=window_sz
+    )
+
+    for metric in ['em', 'mv']:
+        # Train model
+        det = model(**hps[metric]['hps'])
+        det.fit(train)
+
+        # Predict scores
+        scores = det.predict_anomaly_scores(test)
+
+        # Reduce scores if using windows
+        if det_cfg['window']:
+            scores = reduce_window_scores(scores, window_sz)
+
+        # Evaluate performance on test
+        r = evaluate_model(scores, labels)
+        dir_path = MODEL_DIR + f'{detector}/{EXPERIMENT}/{dataset}/'
+        with open(
+            dir_path + f'{metric}.yaml',
+            'w',
+        ) as file:
+            yaml.dump(r, file)
 
 
 def main():
@@ -164,6 +176,7 @@ def main():
     datasets_seasonality = get_dataset_seasonality()
     optuna.logging.set_verbosity(optuna.logging.WARNING)
 
+    # Parallel optimization
     tasks = []
     for detector in automl_cfg['detectors']:
         for dataset in datasets:
@@ -175,6 +188,14 @@ def main():
             total=len(tasks),
         ):
             save_result(result)
+
+    # Load models, best hyperparams and evaluate using supervised metrics
+    with multiprocessing.Pool(MAX_WORKERS) as p:
+        for result in tqdm.tqdm(
+            p.imap_unordered(process_evaluation, tasks[:1]),
+            total=len(tasks[:1]),
+        ):
+            continue
 
 
 if __name__ == '__main__':
